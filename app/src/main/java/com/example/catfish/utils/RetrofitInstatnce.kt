8ff1af5abcb794d43be915424d8ae92006c5a3c1
jobkilonzo <jobkilonzo95@gmail.com>
2025-05-24 17:07:42 +0300

    Inital commit

--- app/src/main/java/com/example/catfish/utils/RetrofitInstatnce.kt
+++ app/src/main/java/com/example/catfish/utils/RetrofitInstatnce.kt
@@ -1,10 +1,13 @@
 package com.example.catfish.utils
 
 import com.example.catfish.data.ApiInterface
+import retrofit2.Retrofit
 
 object RetrofitInstatnce {
     ///fact
     val api: ApiInterface by lazy {
-        //Retrofit
+        Retrofit.Builder()
+            .baseUrl(Util.Base)
+            .addConverterFactory()
     }
 }
